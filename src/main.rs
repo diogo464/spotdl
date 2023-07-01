a9070626ddd98113b17386efa02034ee88092063
diogo464 <diogo464@protonmail.com>
2023-07-01 12:16:42 +0100

    Revert "feat: dont write updated metadata if it is unchanged"
    This reverts commit 68b37debad8835015e7eddb9e995cc5eff81b26b.
    Turns out that single comparisson doesn't quite work all the time and I
    haven't figured out why so I am reverting it for now.

--- src/main.rs
+++ src/main.rs
@@ -792,7 +792,6 @@ async fn subcmd_update(args: UpdateArgs) -> Result<()> {
                     } else {
                         id3::Tag::read_from_path(&file).unwrap_or_default()
                     };
-                    let prev_tag = tag.clone();
                     spotdl::tag::fetch_metadata_to_existing_tag_with(
                         &mut tag,
                         track.rid.id,
@@ -800,10 +799,6 @@ async fn subcmd_update(args: UpdateArgs) -> Result<()> {
                         &fetch_metadata_params,
                     )
                     .await?;
-                    if tag == prev_tag {
-                        tracing::debug!("no metadata changes for {}", file.display());
-                        continue;
-                    }
                     if file.extension() == Some(OsStr::new("wav")) {
                         tag.write_to_wav_path(&file, id3::Version::Id3v24)
                             .with_context(|| {
